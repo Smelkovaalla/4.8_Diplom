cb3a3870832a7b039dd42590fc782ca161264ebb
Alla Smelkova <87067294+Smelkovaalla@users.noreply.github.com>
2021-11-15 08:56:40 +0000

    Готова работа чат бота и поиск фото, начинаю базу данных

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 import json
+from random import randint
 
 
 # Работа с ВК
@@ -20,38 +21,27 @@ class VkUser:
     self.UsersInfo_url = self.url + 'users.get'
     self.UsersInfo_params = {
       'user_ids': id_client,
-      'fields': 'sex, city, status, relation, bdate'
+      'fields': 'sex, city, status, relation, bdate, country'
       }
     req = requests.get(self.UsersInfo_url, params={**self.params, **self.UsersInfo_params}).json()
+    req = req['response'][0]
     # pprint(req)
-
-    if 'sex' in req['response'][0]:
-      client_sex = req['response'][0]['sex']
-    else:
-      client_sex = ''
+    client_info = {}
+    if 'sex' in req:
+      client_info['sex'] = req['sex']
   #   пол. Возможные значения:
   # 1 — женский;
   # 2 — мужской;
   # 0 — пол не указан.
-
-    if 'title' in req['response'][0]['city']:
-      client_city = req['response'][0]['city']['title']
-    else:
-      client_city = ''
-
-    if 'bdate' in req['response'][0]:
-      client_bdate = req['response'][0]['bdate']
-      client_info = {}
+    if 'city' in req.keys():
+      client_info['city'] = req['city']['title']
+    if 'bdate' in req:
+      client_bdate = req['bdate']
       client_bdate = str(client_bdate)
       bdate = client_bdate[-4:]
-      bdate = int(bdate)
-    else:
-      bdate = ''
-
-    if 'relation' in req['response'][0]:
-      client_relation = req['response'][0]['relation']
-    else:
-      client_relation = ''
+      client_info['bdate'] = int(bdate)
+    if 'relation' in req:
+      client_info['status'] = req['relation']
   #   семейное положение. Возможные значения:
   # 1 — не женат/не замужем;
   # 2 — есть друг/есть подруга;
@@ -62,64 +52,42 @@ class VkUser:
   # 7 — влюблён/влюблена;
   # 8 — в гражданском браке;
   # 0 — не указано.
-    # print(client_sex, client_city, client_bdate, client_relation)
-    client_info = {}  
-    client_info['sex'] = client_sex
-    client_info['city'] = client_city
-    client_info['bdate'] = bdate
-    client_info['status'] = client_relation
     return client_info
 
-# Если информации недостаточно, запрашиваем необходимую.
-  def UsersInfo_all(self, client_info):
-      
-    client_info_all = client_info
-
-    if client_info['bdate'] == '':
-      bdate = input('Введите год рождения')
-      client_info_all['bdate'] = bdate
-
-    if client_info['sex'] == '':
-      sex = input('Выберите и впишите индекс вашего пола. (1-женщина, 2-мужчина)')
-      client_info_all['sex'] = sex
-
-    if client_info['status'] == '':
-      print('Введите статус вашего семейного положения? 1-не женат/не замужем; 2 — есть друг/есть подруга; 3 — помолвлен/помолвлена; 4 — женат/замужем; 5 — всё сложно; 6 — в активном поиске; 7 — влюблён/влюблена; 8 — в гражданском браке;')
-      status = input()
-      if status == '1' or status == '5' or status == '6':
-        client_info_all['status'] = status
-      elif status == '2' or status == '3' or status == '4' or status == '7' or status == '8':
-        print('Иди к своей половинке')
-      else:
-        print('Вы ввели неверное значение')
-
-
-    if client_info['city'] == '':
-      city = input('Введите название вашего города')
-      client_info_all['city'] = city
-
-    return client_info_all
+  def search_city(self):
+    self.search_city_url = self.url + 'database.getCities'
+    self.search_city_params = {
+      'count':1000,
+      'country_id':1,
+      'need_all':0
+    }
+    req = requests.get(self.search_city_url, params={**self.params, **self.search_city_params}).json() 
+    city_list = []
+    for i in req['response']['items']:
+      a = i['title'].lower()
+      city_list.append(a)
+    return city_list
 
 # Подбираем параметры для пары клиента и ищем 3 подходящих кандидатов 
-  def search_pare(self, client_info_all):
+  def search_pare(self, client_info):
     pare_info = {}
-    pare_info['status'] = '1'
-    pare_info['city'] = client_info_all['city']
-    if client_info_all['sex'] == 1:
+    pare_info['status'] = '6'
+    pare_info['city'] = client_info['city']
+    if client_info['sex'] == 1:
       pare_info['sex'] = '2'
-    elif client_info_all['sex'] == 2:
+    elif client_info['sex'] == 2:
       pare_info['sex'] = '1'
-    if client_info_all['sex'] == 1:
-      pare_info['age_from'] = 2021 - int(client_info_all['bdate'])
-      pare_info['age_to'] = 2021 - int(client_info_all['bdate']) + 5
-    elif client_info_all['sex'] == 2:
-      pare_info['age_from'] = 2021 - int(client_info_all['bdate']) - 5
-      pare_info['age_to'] = 2021 - int(client_info_all['bdate'])
+    if client_info['sex'] == 1:
+      pare_info['age_from'] = 2021 - int(client_info['bdate'])
+      pare_info['age_to'] = 2021 - int(client_info['bdate']) + 5
+    elif client_info['sex'] == 2:
+      pare_info['age_from'] = 2021 - int(client_info['bdate']) - 5
+      pare_info['age_to'] = 2021 - int(client_info['bdate'])
     # print(pare_info)
 
     self.search_id_url = self.url + 'users.search'
     self.search_id_params = {
-      'count': 3,
+      'count': 100,
       'sex': pare_info['sex'],
       'hometown': pare_info['city'],
       'status': pare_info['status'],
@@ -127,18 +95,16 @@ class VkUser:
       'age_to' : pare_info['age_to']
     }
     req = requests.get(self.search_id_url, params={**self.params, **self.search_id_params}).json() 
-    pare1 = req['response']['items'][0]['id']
-    pare2 = req['response']['items'][1]['id']
-    pare3 = req['response']['items'][2]['id']
-    pare = [pare1, pare2, pare3]
+    m = randint(0, 100)
+    pare = req['response']['items'][m]['id']
     return pare
 
 # Поиск фото по id аккаунта, сортировка фото по популярности
-  def search_photos(self, owner_id, sorting=0):
+  def search_photos(self, pare_id, sorting=0):
     self.photos_search_url = self.url + 'photos.get'
     self.photos_search_params = {
       'count': 50,
-      'owner_id': owner_id,
+      'owner_id': pare_id,
       'extended': 1,
       'album_id': 'profile'
     }
@@ -172,73 +138,96 @@ if __name__ == "__main__":
   vk_group = vk_api.VkApi(token=token_VK_group)
   vk_session = vk_group.get_api()
   longpoll = VkLongPoll(vk_group)
+  
   vk_client = VkUser(token_VK)
 
 
 
-  id_client = 135378796
+  # id_client = 135378796
 
-  def search_pare_photos(id_client, vk_client):
-    client_info = vk_client.UsersInfo(id_client)
-    # pprint(client_info)
+  # def search_pare_photos(id_client, vk_client):
+  #   client_info = vk_client.UsersInfo(id_client)
+  #   # pprint(client_info)
 
-    client_info_all = vk_client.UsersInfo_all(client_info)
-    # pprint(client_info_all)
+  #   client_info_all = vk_client.UsersInfo_all(client_info)
+  #   # pprint(client_info_all)
 
-    pare_info = vk_client.search_pare(client_info_all)
-    # pprint(pare_info)
-
-    url_pare = 'https://vk.com/id' + str(pare_info[0])
-
-    photos_dict = vk_client.search_photos(pare_info[0])
-    # pprint(photos_dict)
+  def search_pare_photos(client_info):
+    pare_id = vk_client.search_pare(client_info)
+    pprint(pare_id)
+    url_pare = 'https://vk.com/id' + str(pare_id)
+    photos_dict = vk_client.search_photos(pare_id)
+    pprint(photos_dict)
     sms = []
     sms.append(url_pare)
     for i in photos_dict:
       sms.append(i[0])
+    print(sms)
     return(sms)
 
-  pprint(search_pare_photos(id_client, vk_client))
-
+  # pprint(search_pare_photos(id_client))
 
     # Создадим функцию для ответа на сообщения в лс группы
   def blasthack(id, text):
       vk_group.method('messages.send', {'user_id' : id, 'message' : text, 'random_id': 0})
 
-#  Слушаем longpoll(Сообщения)
+  # def answer(event_id, user_id, peer_id, event_data):
+  #     vk_group.method('messages.sendMessageEventAnswer', {
+  #       'event_id' : event_id, 
+  #       'user_id' : user_id, 
+  #       'peer_id' : peer_id, 
+  #       'event_data' : event_data})
+
+  
+  # city_list = ['санкт-петербург', 'москва']
+  client_info_all = {}
+# #  Слушаем longpoll(Сообщения)
   for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
-
       # Чтобы наш бот не слышал и не отвечал на самого себя
       if event.to_me:
-
         # Для того чтобы бот читал все с маленьких букв 
         message = event.text.lower()
-
         # Получаем id пользователя
-        id_client = event.user_id
-
+        user_id = event.user_id
+        client_info = vk_client.UsersInfo(user_id)
+        city_list = vk_client.search_city()
+        pprint(city_list)
         # Структура переписки
         if message == 'привет':
-          blasthack(id_client, 'Для подбора пары введите 1')
+          client_info_all = client_info
+          if len(client_info) == 4:             
+            blasthack(user_id, 'Ваши данные полные, введите "найди пару"')  
+          elif len(client_info) == 3:
+            if 'sex' not in client_info:
+              blasthack(user_id, 'Ваш пол не установлен, введите ж или м')            
+            elif 'city' not in client_info:
+              blasthack(user_id, 'Введите название вашего города')
+            elif 'bdate' not in client_info:
+              blasthack(user_id, 'Введите год вашего рождения')  
+            elif client_info_all['status'] == '0':
+              client_info_all['status'] = '6'
+              blasthack(user_id, 'Ваши данные полные, введите "найди пару"')                       
+        elif message == 'ж':
+          client_info_all['sex'] = 1        
+          blasthack(user_id, 'Ваши данные полные, введите "найди пару"')  
+        elif message == 'м':
+          client_info_all['sex'] = 2        
+          blasthack(user_id, 'Ваши данные полные, введите "найди пару"')  
+        elif message in city_list:
+          client_info_all['city'] = message
+          blasthack(user_id, 'Ваши данные полные, введите "найди пару"')    
+        elif len(str(message)) == 4:
+          client_info_all['bdate'] = message
+          blasthack(user_id, 'Дата рождения принята, Ваши данные полные, введите "найди пару"')          
           
-        elif message == '1':
-          text = search_pare_photos(id_client, vk_client)
+        elif message == 'найди пару':    
+          text = search_pare_photos(client_info_all)
           for i in text:
             sms = str(i)
-            blasthack(id_client, sms)
-       
-        elif message == 'как дела?':
-          blasthack(id_client, 'Хорошо, а твои как?')
-
+            blasthack(user_id, sms)
         else:
-          blasthack(id_client, 'Я вас не понимаю! :(')
-
-
-
-
-
-
+          blasthack(user_id, 'Я вас не понимаю')  
 
 
 
